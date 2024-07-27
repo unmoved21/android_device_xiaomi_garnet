f4d5557f8a96b4159d3e7aa59bca6509ab2f517d
Arian <arian.kulmer@web.de>
2024-07-27 15:11:24 +0200

    garnet: sensors: Convert nonui notifier into a generalized sensor based notifier
    Change-Id: I34b9861341f4f033185fdbd066b46d779dd95cfa

--- device.mk
+++ device.mk
@@ -353,6 +353,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal:64 \
     sensors.xiaomi.v2:64
 
+PRODUCT_PACKAGES += \
+    sensor-notifier
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
@@ -417,9 +420,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
 # Touchscreen
-PRODUCT_PACKAGES += \
-    nonui-notifier
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
