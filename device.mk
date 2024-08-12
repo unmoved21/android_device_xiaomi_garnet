b2dce0412d71c3d41e3ff78ac6d4c08bc7a0e828
Arian <arian.kulmer@web.de>
2024-08-12 12:39:34 +0200

    garnet: Stop building libcodec2_vndk explicitly
    It's a dependency of libcodec2_hidl@1.0
    
    Change-Id: I7a14b3b23d49ce3f0f1a1497005085e6f4f4224f

--- device.mk
+++ device.mk
@@ -328,7 +328,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libOmxCore \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor \
     libstagefrighthw \
     libstagefright_softomx_plugin.vendor
 
