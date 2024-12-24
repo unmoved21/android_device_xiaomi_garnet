90df973916c8ab8929970aa70f6a9643c8804416
UnmoveD <unmoved2121@gmail.com>
2024-12-24 19:08:12 +0100

    garnet: Add missing security dependencies in keymint service
    out/soong/.intermediates/vendor/xiaomi/garnet/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_cortex-a76_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security11secureclock13BnSecureClock12createBinderEv
    
    out/soong/.intermediates/vendor/xiaomi/garnet/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_cortex-a76_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security11secureclock13BnSecureClock19getInterfaceVersionEPi
    
    out/soong/.intermediates/vendor/xiaomi/garnet/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_cortex-a76_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security12sharedsecret14BnSharedSecret12createBinderEv
    
    Change-Id: I89b974c72d9a935e4ba5a69ffa90a429acbbd0fb

--- extract-files.py
+++ extract-files.py
@@ -71,7 +71,19 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib/libqtikeymint.so',
         'vendor/lib64/libqtikeymint.so',
     ): blob_fixup()
-        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+        .replace_needed(
+            'android.hardware.security.keymint-V1-ndk_platform.so',
+            'android.hardware.security.keymint-V1-ndk.so',
+        )
+        .replace_needed(
+            'android.hardware.security.secureclock-V1-ndk_platform.so',
+            'android.hardware.security.secureclock-V1-ndk.so',
+        )
+        .replace_needed(
+            'android.hardware.security.sharedsecret-V1-ndk_platform.so',
+            'android.hardware.security.sharedsecret-V1-ndk.so',
+        )
+        .add_needed('android.hardware.security.rkp-V1-ndk.so'),
     'vendor/etc/camera/pureView_parameter.xml': blob_fixup()
         .regex_replace(r'=([0-9]+)>', r'="\1">'),
     (
