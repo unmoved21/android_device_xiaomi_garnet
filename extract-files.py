6bdeaf02aab9c17a48a4ad9133cbd3b71445c23e
rk134 <rahul@aospa.co>
2025-02-02 18:48:09 +0000

    garnet: Add Dolby Atmos
    * pong-development/hardware_dolby

--- extract-files.py
+++ extract-files.py
@@ -113,10 +113,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('setsockopt: 1'),
     'vendor/etc/media_codecs_parrot_v0.xml': blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
-    'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
-        .regex_replace('.+dolby.+\n', ''),
-    'vendor/etc/media_codecs_c2_audio.xml': blob_fixup()
-        .regex_replace('.+media_codecs_dolby_audio.+\n', ''),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
     'vendor/etc/perf/commonresourceconfigs.xml': blob_fixup()
